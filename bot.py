f38d8ed95d7a8aae10bfee416501c414e9ec5847
Jonathan Schall <62366496+JaySchall@users.noreply.github.com>
2023-07-26 17:56:57 -0400

    Added updating emojis with command

--- bot.py
+++ bot.py
@@ -7,20 +7,35 @@ import settings
 
 def run_bot():
     intents = discord.Intents.default()
-
     client = discord.Client(intents=intents)
-
+    tree = app_commands.CommandTree(client)
+    
+    
     @client.event
     async def on_ready():
         print("HAIII")
+        #synced = await tree.sync()
+        #print(f"Synced {len(synced)} commands")
     
     @client.event
     async def on_message(message):
-        if message.author.id == 393827547873280000: #Clair's ID
-            emojiNumber = random.randint(0,1)
+        if message.author.id == 209440277981560833: #Not Clair's ID
+            emojiNumber = random.randint(0,len(settings.bearEmojis) - 1)
             await message.add_reaction(settings.bearEmojis[emojiNumber])
             print("reacted")
     
+    @tree.command(name="updateemojis", description="updates the list of emojis")
+    @app_commands.checks.has_permissions(administrator = True)
+    async def bearemojis(interaction: discord.Interaction):
+        guild = client.get_guild(settings.emojiServer)
+        f = open(settings.emojiList, "w")
+        for emoji in guild.emojis:
+            if "bear" in emoji.name:
+                f.write(str(emoji) + "\n")
+        f.close()
+        await settings.updateEmojiList()
+        await interaction.response.send_message("Updated List")
+
     client.run(settings.TOKEN)
 if __name__ == "__main__":
     run_bot()
--- settings.py
+++ settings.py
@@ -4,4 +4,18 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+emojiServer = 795084804181065768
+
+emojiList = "emojis.txt"
+
 bearEmojis = ["<:pinkteddy:1133624059205271553>","<:bearLove:1133624085339983882>"]
+
+async def updateEmojiList():
+    global bearEmojis
+    bearEmojis = []
+    f = open(emojiList, "r")
+    for line in f:
+        line = line.strip()
+        bearEmojis.append(line)
+    print(bearEmojis)
+    f.close()
